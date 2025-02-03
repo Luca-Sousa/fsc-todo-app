847c82428c42724c91d28468bfb813a2ae583334
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-02-03 09:40:13 -0300

    feat: add Sidebar and SidebarButton component

---
+++ src/components/SidebarButton.jsx
@@ -0,0 +1,19 @@
+const SidebarButton = ({ children, variant }) => {
+  const getVariantClass = () => {
+    if (variant === "unselected") {
+      return "text-brand-dark-blue"
+    }
+
+    if (variant === "selected") {
+      return "text-brand-primary bg-brand-primary/20"
+    }
+  }
+
+  return (
+    <a href="#" className={`rounded-lg px-6 py-3 ${getVariantClass()}`}>
+      {children}
+    </a>
+  )
+}
+
+export default SidebarButton
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode></React.StrictMode>
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+import Sidebar from "./components/sidebar"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Sidebar />
+  </React.StrictMode>
 )
