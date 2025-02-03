5f298c987e8c452231d535f8f97f3dc594195dbd
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-02-03 09:54:40 -0300

    feat: add icons svgr to Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import HomeIcon from "../assets/icons/home.svg?react"
+import TaskIcon from "../assets/icons/tasks.svg?react"
 import SidebarButton from "./SidebarButton"
 
 const Sidebar = () => {
@@ -14,8 +16,14 @@ const Sidebar = () => {
       </div>
 
       <div className="flex flex-col gap-2 p-2">
-        <SidebarButton variant="unselected">Início</SidebarButton>
-        <SidebarButton variant="selected">Minhas Tarefas</SidebarButton>
+        <SidebarButton variant="unselected">
+          <HomeIcon />
+          Início
+        </SidebarButton>
+        <SidebarButton variant="selected">
+          <TaskIcon />
+          Minhas Tarefas
+        </SidebarButton>
       </div>
     </div>
   )
