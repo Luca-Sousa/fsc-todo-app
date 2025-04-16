93060c9e15db078de6e6cf48be7199395bc45eb9
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-04-16 09:31:31 -0300

    feat: add delete loading state to task item

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -76,17 +76,7 @@ const Tasks = () => {
     toast.success("Tarefa adicionada com sucesso!")
   }
 
-  const handleTaskDeleteClick = async (taskId) => {
-    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
-      method: "DELETE",
-    })
-
-    if (!response.ok) {
-      return toast.success(
-        "Erro ao deletar a tarefa. Por favor, tente novamente."
-      )
-    }
-
+  const onDeleteTaskSucess = async (taskId) => {
     const newTasks = tasks.filter((task) => task.id !== taskId)
     setTasks(newTasks)
     toast.success("Tarefa deletada com sucesso!")
@@ -130,7 +120,7 @@ const Tasks = () => {
               key={task.id}
               task={task}
               handleCheckboxClick={handleTaskCheckboxClick}
-              handleDeleteClick={handleTaskDeleteClick}
+              onDeleteSucess={onDeleteTaskSucess}
             />
           ))}
         </div>
@@ -143,7 +133,7 @@ const Tasks = () => {
               key={task.id}
               task={task}
               handleCheckboxClick={handleTaskCheckboxClick}
-              handleDeleteClick={handleTaskDeleteClick}
+              onDeleteSucess={onDeleteTaskSucess}
             />
           ))}
         </div>
@@ -156,7 +146,7 @@ const Tasks = () => {
               key={task.id}
               task={task}
               handleCheckboxClick={handleTaskCheckboxClick}
-              handleDeleteClick={handleTaskDeleteClick}
+              onDeleteSucess={onDeleteTaskSucess}
             />
           ))}
         </div>
